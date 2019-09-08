5c119197cbf2c8e63d984284e43c6c583434c410
José Ángel <joseangelmt@outlook.com>
2019-09-08 19:02:06 +0200

    Changes dataPath to run in Windows machine.

--- src/FinalProject_Camera.cpp
+++ src/FinalProject_Camera.cpp
@@ -28,7 +28,7 @@ int main(int argc, const char *argv[])
     /* INIT VARIABLES AND DATA STRUCTURES */
 
     // data location
-    string dataPath = "../";
+    string dataPath = "../../../";
 
     // camera
     string imgBasePath = dataPath + "images/";
@@ -132,7 +132,7 @@ int main(int argc, const char *argv[])
         bVis = true;
         if(bVis)
         {
-            show3DObjects((dataBuffer.end()-1)->boundingBoxes, cv::Size(4.0, 20.0), cv::Size(2000, 2000), true);
+            show3DObjects((dataBuffer.end()-1)->boundingBoxes, cv::Size(4.0, 20.0), cv::Size(640, 480), true);
         }
         bVis = false;
 
